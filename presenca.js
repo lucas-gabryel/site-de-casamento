edb47f046bfda69c9d86d07ea3a7aad2b4b5f4db
Lucas Gabryel <lucas-gabryel@outlook.com>
2025-02-01 00:23:24 -0300

    Permitir CORS

--- presenca.js
+++ presenca.js
@@ -4,20 +4,14 @@ async function verificarPresenca(event) {
     const nomeCompleto = document.getElementById('name').value.trim();
     const resultado = document.getElementById('resultado');
   
-    console.log('Nome digitado:', nomeCompleto); // Debug
-  
     if (!nomeCompleto) {
       resultado.textContent = "Por favor, insira seu nome completo.";
       return;
     }
   
     try {
-      // URL do Google Apps Script (substitua pelo seu URL)
       const url = 'https://script.google.com/macros/s/AKfycbzIjI1_HinciDUe2C3Lg46MAkG27B17Ii4ZdPmV7qErx31FH1rFB14e2vM55fuUFgj8/exec';
   
-      console.log('Enviando requisição para:', url); // Debug
-  
-      // Envia os dados para o Google Apps Script
       const response = await fetch(url, {
         method: 'POST',
         body: JSON.stringify({ nome: nomeCompleto }),
@@ -26,10 +20,8 @@ async function verificarPresenca(event) {
         },
       });
   
-      console.log('Resposta recebida:', response); // Debug
-  
-      const message = await response.text();
-      resultado.textContent = message; // Exibe a mensagem de confirmação ou erro
+      const data = await response.json(); // Parse da resposta JSON
+      resultado.textContent = data.message; // Exibe a mensagem de confirmação ou erro
     } catch (error) {
       console.error('Erro ao verificar presença:', error);
       resultado.textContent = "Erro ao verificar presença. Tente novamente mais tarde.";
